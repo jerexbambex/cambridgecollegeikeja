13167099af096bded14d2a92128f3b32599e027d
Jeremiah <jerexbambex@gmail.com>
2020-09-13 23:02:07 +0100

    Edited the file extension for some images

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function index()
+    {
+        $events = Event::all();
+
+        return view('frontend.events');
+    }
 }
